e2f0b6bc24d24fe03cf77c99545b04679f36f807
paulapivat <paul.apivat@gmail.com>
2019-02-13 00:01:38 +0700

    add getBlock method

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 require('dotenv').config();
 var BigNumber = require('bignumber.js');
-var Web3 = require('web3')
+const Web3 = require('web3')
 
-var web3 = new Web3(new Web3.providers.HttpProvider(process.env.API_URL));
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.API_URL));
 
 var balance = web3.eth.getBalance(process.env.ACCOUNT, function (error, result) {
     if (!error) {
-	console.log(web3.utils.fromWei(new BigNumber(result).toString(), 'ether'));
+	console.log('Account Balance:', web3.utils.fromWei(new BigNumber(result).toString(), 'ether'));
     } else {
       console.error(error);
     }
   });
 
 // add .toString() to BigNumber due to error
+
+let block = web3.eth.getBlock(7211516, function(error, result){
+  if(!error)
+      console.log(JSON.stringify(result));
+  else
+      console.error(error);
+})
+
+
+
+// CREATE - generates an account object
+// https://web3js.readthedocs.io/en/1.0/web3-eth-accounts.html
+
+// let newAcc = web3.eth.accounts.create();
+// console.log(newAcc);
+
+// web3.eth.getProtocolVersion().then(console.log)
+console.log("protocol version:", web3.eth.getProtocolVersion().then(console.log));
 
